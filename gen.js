676e65bb76da4568ea7093f4f6613055d03473ca
JernKunpittaya <61564542+JernKunpittaya@users.noreply.github.com>
2023-03-29 17:56:52 +0700

    tablesub

--- gen.js
+++ gen.js
@@ -63,6 +63,58 @@ async function generateTableAll(regex, circuitLibPath, circuitName) {
   });
   writeStream.end();
 }
+// every substring
+async function generateTableSub(
+  regex,
+  substrings,
+  circuitLibPath,
+  circuitName
+) {
+  const ast = regexpTree.parse(`/${regex}/`);
+  regexpTree.traverse(ast, {
+    "*": function ({ node }) {
+      if (node.type === "CharacterClass") {
+        throw new Error("CharacterClass not supported");
+      }
+    },
+  });
 
-let regex = "a(1|3|4)*d";
+  const graph_json = lexical.compile(regex);
+  const N = graph_json.length;
+  // substrings = [{min: _, max: _, trans: [[from1, to1], [from2, to2], ...] }, {min: _, max: _, trans: [[from1, to1], [from2, to2], ...]}, ...]
+  let writeStream;
+  // go through each substring
+  let min = 0;
+  // last state of previous variable parts
+  let last_state = 0;
+  let max = 0;
+  for (let i = 0; i < substrings.length; i++) {
+    writeStream = fs.createWriteStream("table_sub_" + i + ".txt");
+    min += substrings[i]["trans"][0][0] - last_state;
+    max += substrings[i]["trans"][0][0] - last_state + substrings[i]["max"] - 1;
+    writeStream.write(substrings[i]["max"] + "\n");
+    writeStream.write(min + "\n");
+    writeStream.write(max + "\n");
+    // console.log("afdsa ", substrings[i]["trans"]);
+    substrings[i]["trans"].forEach((tran) => {
+      writeStream.write(tran[0] + " " + tran[1] + "\n");
+      if (tran[1] < last_state) {
+        last_state = tran[1];
+      }
+    });
+    writeStream.end();
+    // update min
+    min += substrings[i]["min"] - 1;
+  }
+}
+
+let regex = "M(1|2|3|4|5)*(a|v|d|u)*t";
 generateTableAll(regex);
+generateTableSub(regex, [
+  {
+    min: 1,
+    max: 5,
+    trans: [[1, 1]],
+  },
+  { min: 2, max: 4, trans: [[2, 2]] },
+]);
